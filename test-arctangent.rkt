724529db46fc74c3e02097b429442c16dcb82d8b
Danny Yoo <dyoo@cs.wpi.edu>
2011-07-12 22:58:26 -0400

    added beginner test suite

--- test-arctangent.rkt
+++ test-arctangent.rkt
@@ -1,39 +1,110 @@
-#lang planet dyoo/arctangent
+#lang racket/base
 
-(= foo 13)
-foo
-'foo
+(require racket/sandbox
+         racket/runtime-path
+         racket/list
+         rackunit
+         syntax/strip-context)
 
+(define-runtime-path language.rkt "language.rkt")
 
+(define my-eval #f)
+(define output-port #f)
+(define error-port #f)
 
-(+ (+ 1 2) (+ 3 (+ 4 5)))
+(define (init-my-eval!)
+  ;;(call-with-trusted-sandbox-configuration
+  ;; (lambda ()
+     (set! output-port (open-output-string))
+     (set! error-port (open-output-string))
+     (parameterize ([sandbox-output output-port]
+                    [sandbox-error-output error-port])
+       (set! my-eval (make-evaluator `(file ,(path->string language.rkt))))))
+;))
 
-(cons 'f '(a b))
-(= f '(a b))
-
-
-
-(= z (cons (cons 3 4) 5))
-(= (car (car z)) 'three)
-z
-
-(= (cdr z) 'five)
-z
-
-(def average (x y)
-  (/ (+ x y) 2))
-
-average
-
-(fn (x y) (/ (+ x y) 2))
+(init-my-eval!)
 
 
-(= a (fn (x y) (/ (+ x y) 2)))
+(define (run-tests tests)
+  (let loop ([last-result #f]
+             [tests (syntax->list tests)])
+    (cond
+      [(empty? tests)
+       (void)]
+      [(eq? (syntax->datum (first tests)) '---)
+       (init-my-eval!)
+       (loop #f (rest tests))]
+      [(eq? (syntax->datum (first tests)) '==>)
+       (let ([src (first tests)]
+             [expected (syntax->datum (second tests))])
+         (with-check-info* (list (make-check-location (list (syntax-source src)
+                                                            (syntax-line src)
+                                                            (syntax-column src)
+                                                            (syntax-position src)
+                                                            (syntax-span src))))
+                           (lambda ()
+                             (check-equal? last-result expected))))
+       (loop #f (rest (rest tests)))]
+      [else
+       (let ([next-result (my-eval (first tests))])
+         (loop next-result (rest tests)))])))
+      
 
-((fn (x y) (/ (+ x y) 2)) 2 4)
 
-"hello"
-("hello" 3)
+(define program-tests
+  (strip-context
+   #'((= foo 13)
+      foo   ==>   13
+      'foo  ==>   foo
+      
+      ---
+      
+      (+ (+ 1 2) 
+         (+ 3 (+ 4 5)))   ==> 15
+      
+      ---
+      
+      (cons 'f '(a b))    ==> (f a b)
+      (= f '(a b))
+      f                   ==> (a b)
+            
+      ---
+      
+      
+      (= z (cons (cons 3 4) 5))
+      (= (car (car z)) 'three)
+      z                   ==> ((three . 4) . 5)
+      
+      (= (cdr z) 'five)
+      z                   ==> ((three . 4) . five)
+      
+      ---
+      
+      (def average (x y)
+        (/ (+ x y) 2))
+      
+      (average 2 4)       ==> 3
+      
+      ((fn (x y) (/ (+ x y) 2)) 2 4) ==> 3
+      
+      
+      
+      (= a (fn (x y) (/ (+ x y) 2)))
+  
+      (a 17 0) ==> 17/2
+      
+      "hello"
+      ("hello" 3)   ==> #\l
+      ("hello" 4)   ==> #\o
+      ---
 
-(= message "Hello world")
-;(= (message 2) #\x)
+      
+      (= message "Hello world")
+      (= (message 2) #\x)
+      message         ==> "Hexlo world"
+      
+      
+      )))
+
+  
+(run-tests program-tests)
